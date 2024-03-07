6697da47259bd5bc473c35ddaf3e5293ff5e4511
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-03-07 17:39:10 +0000

    Markdown to HTML Conversion, Entry Pages, Updated Index Page, Search Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:page>", views.entry_page, name="page"),
+    path("search", views.search_page, name="search"),
+    path("new_page", views.create_new_page, name="new_page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,56 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from markdown2 import Markdown
 
 from . import util
 
+
+def markdown_to_html(entry):
+    content = util.get_entry(entry)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry_page(request, page):
+    html = markdown_to_html(page)
+    if html == None:
+        return render(request, "encyclopedia/error.html",{
+            "title": page,
+            "msg":"This page does not exist."
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": page,
+            "content": html
+        })
+    
+def search_page(request):
+    query = request.GET.get('q', '').lower()
+    search_results = []
+    for entry in util.list_entries():
+        if  query in entry.lower():
+            search_results.append(entry)
+
+    match = None
+    for entry in search_results:
+        if entry.lower() == query:
+            match = entry
+            break
+
+    if match:
+        return redirect("page", page=match)
+        
+    else:
+        return render(request, "encyclopedia/search.html", {
+            "entries" : search_results
+        })
+    
+def create_new_page(request):
+    return render(request, 'encyclopedia/new_page.html')
