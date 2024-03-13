5d1ac1d0be9c129c99463a7e7eb0ff9db09b3e28
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-03-13 16:53:03 +0000

    Finished edit function and finished random page functionality. Corrected some wrong paths

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:page>", views.entry_page, name="page"),
-    path("wiki/search", views.search_page, name="search"),
+    path("search", views.search_page, name="search"),
     path("new", views.new_page, name="new"),
-    path("wiki/<str:page>/edit/", views.edit_page, name="edit")
+    path("wiki/<str:page>/edit/", views.edit_page, name="edit"),
+    path("random", views.random_page, name="random"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from markdown2 import Markdown
+import random
 
 from . import util
 
@@ -20,7 +21,6 @@ def index(request):
 
 def entry_page(request, page):
     html = markdown_to_html(page)
-    print(html)
     if html == None:
         return render(request, "encyclopedia/error.html",{
             "title": "Error",
@@ -57,7 +57,7 @@ def new_page(request):
     if request.method == "POST":
         title = request.POST.get("new_page_title")
         description = request.POST.get("new_page_description")
-        content = f"#{title}\n{description}"
+        content = f"#{title}\n\n{description}"
         entries = util.list_entries()
         if title in entries:
             return render(request, "encyclopedia/error.html",{
@@ -69,8 +69,20 @@ def new_page(request):
             return redirect("page", page=title)
     return render(request, 'encyclopedia/new_page.html')
 
-def edit_page(request, page):    
+def edit_page(request, page):
+    description = '\n'.join(util.get_entry(page).split('\n')[2:])
+    print(description)
+    if request.method == "POST":
+        description = request.POST.get("edit_page_description")
+        content = f"#{page}\n\n{description}"
+        util.save_entry(page, content)
+        return redirect("page", page=page)
     return render(request, "encyclopedia/edit_page.html",{
-        "edit_page_description" : '\n'.join(util.get_entry(page).split('\n')[2:]),
+        "edit_page_description" : description,
         "edit_page_title": page
     })
+
+def random_page(request):
+    entries = util.list_entries()
+    random_page = random.choice(entries)
+    return redirect("page", page=random_page)
